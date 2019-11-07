16f2379cbb43a77ad0f28bd39b7167e8684c72d9
Alvaro Alonso <alvaroalonsoflor@gmail.com>
2019-11-07 20:32:05 +0000

    Create function to calculate ending character of a single event.

---
+++ stellaris-configuration-jsonifier/src/models/event_parser.js
@@ -0,0 +1,30 @@
+/**
+ * This function finds the ending bracket given a string where the begining bracket has been found
+ * @param slicedEventString
+ * @param eventStartingIndex
+ * @returns {number}, ending bracket index
+ */
+const determineEventEndingCharLocation = (slicedEventString, eventStartingIndex) => {
+
+    let bracketsCounter = 0;
+    let endingChar = -1;
+
+    slicedEventString.split('').forEach( (character, index) => {
+
+        if (character === "{") {
+            bracketsCounter++;
+        } else if (character === "}"){
+            bracketsCounter--;
+        }
+
+        if (bracketsCounter === 0) {
+            // returns ending char
+            endingChar = index + eventStartingIndex;
+        }
+
+    });
+
+    return endingChar;
+
+};
+
