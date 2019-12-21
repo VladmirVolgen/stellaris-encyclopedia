841a50621f4726d2d1f6dd09f121ba1ec23451c6
Alvaro Alonso <alvaroalonsoflor@gmail.com>
2019-12-21 15:33:48 +0000

    Add event data and update manifest project name

--- stellaris-encyclopedia-webapp/src/App.js
+++ stellaris-encyclopedia-webapp/src/App.js
@@ -3,15 +3,39 @@ import './App.css';
 import Title from './components/Title';
 import SearchFilter from './components/SearchFilter/SearchFilter';
 import EventList from './components/EventList/EventList';
+import eventList from './data/event-data';
 
-function App() {
-  return (
-    <div className="App">
-      <Title />
-      <SearchFilter />
-      <EventList />
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      eventsData: null,
+      selectedEvents: null
+    }
+  }
+
+  componentDidMount() {
+    const jsonEventList = JSON.parse(eventList);
+    this.setState({eventsData: jsonEventList})
+    // TODO: It will fetch from REST API
+    // fetch('../../test-data-sets/json-output/events-output.json')
+    //   .then(response => response.json())
+    //   .then(data => this.setState({eventsData: JSON.parse(data)}))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Title />
+        <SearchFilter />
+        <EventList />
+      </div>
+    );
+  }
+
+ 
 }
 
 export default App;
---
+++ stellaris-encyclopedia-webapp/src/data/event-data.js
@@ -0,0 +1,28 @@
+// This file will be deleted when the rest api is ready
+// TODO: Delete the file once the rest api is ready
+
+const eventList = `[
+    {
+        "id": "leviathans.9",
+        "title": "= leviathans.9",
+        "description": "d = leviathans.9",
+        "picture": "leviathans.9",
+        "options": []
+    },
+    {
+        "id": "leviathans.4",
+        "title": "= leviathans.4",
+        "description": "d = leviathans.4",
+        "picture": "leviathans.4",
+        "options": []
+    },
+    {
+        "id": "leviathans.6",
+        "title": "= leviathans.6",
+        "description": "d = leviathans.6",
+        "picture": "leviathans.6",
+        "options": []
+    }
+]`;
+
+export default eventList;
