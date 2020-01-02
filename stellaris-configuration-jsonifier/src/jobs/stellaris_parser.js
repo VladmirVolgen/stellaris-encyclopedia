72c733825ef593ea2b41df1a92e95cd97c13cd69
VladmirVolgen <alvaroalonsoflor@gmail.com>
2020-01-02 18:00:16 +0000

    Start refactoring project acording to a more standarized etl structure

---
+++ stellaris-configuration-jsonifier/src/jobs/stellaris_parser.js
@@ -0,0 +1,33 @@
+const {processFiles} = require('../utilities/file_reader');
+const { processEventFile } = require('../processors/event_parser');
+const fs = require('fs');
+const readAppConfiguration = require('../configuration/read_configuration');
+
+const stellarisParser = function() {  
+    //Read config
+    const config = readAppConfiguration();
+    const pathToJsonOutput = config.processedJsonFolder;
+    const eventsPath = config.pathToStellarisEventFiles;
+    
+    const eventFiles = processFiles(eventsPath);
+    
+    // Writes start array to file
+    fs.writeFileSync(pathToJsonOutput, `[`, (err) => {
+        if (err) throw err
+    });
+    
+    // TODO: Step1, create all jsons
+    for (let index = 0; index < eventFiles.length; index++) {
+        processEventFile(eventFiles[index], index);
+    }
+
+    // TODO: Step2, add localisation to jsons 
+    // (2 options, either insert into database or create new json for each language)
+    
+    // Writes end array to file
+    fs.appendFileSync(pathToJsonOutput, `]`, (err) => {
+        if (err) throw err
+    });
+}
+
+module.exports = stellarisParser;
--- stellaris-configuration-jsonifier/src/main.js
+++ stellaris-configuration-jsonifier/src/main.js
@@ -1,30 +1,9 @@
-const {processFiles} = require('./utilities/file_reader');
-const { processEventFile } = require('./processors/event_parser');
-const fs = require('fs');
-const readAppConfiguration = require('./configuration/read_configuration')
+const stellarisParse = require('./jobs/stellaris_parser');
+
+// Calls the jobs
+stellarisParse();
 
 
-const eventsPath = '../test-data-sets/events/';
-
-
-
-//Read config
-const config = readAppConfiguration();
-const pathToJsonOutput = config.processedJsonFolder;
-
-const eventFiles = processFiles(eventsPath);
-
-fs.writeFileSync(pathToJsonOutput, `[`, (err) => {
-    if (err) throw err
-});
-
-for (let index = 0; index < eventFiles.length; index++) {
-    processEventFile(eventFiles[index], index);
-}
-
-fs.appendFileSync(pathToJsonOutput, `]`, (err) => {
-    if (err) throw err
-});
 
 
 
